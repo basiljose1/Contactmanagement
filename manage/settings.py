237214d6d08052a4b2f3ebb46ec64286a23c33dc
Basil Jose <basiljose1@gmail.com>
2015-06-14 20:15:21 +0530

    Update settings.py

--- manage/settings.py
+++ manage/settings.py
@@ -12,19 +12,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = 'contacts/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
