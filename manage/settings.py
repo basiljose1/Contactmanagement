5918bcf57e197c9dd049a0122613e4c9aefe6195
Basil Jose <basiljose1@gmail.com>
2015-06-14 20:09:48 +0530

    Update settings.py

--- manage/settings.py
+++ manage/settings.py
@@ -18,7 +18,7 @@ DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = 'contacts/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
